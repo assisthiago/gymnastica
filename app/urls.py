667473b8cba63c3c3bbe1dd1e49bdc2f72a7777a
Thiago Assis <thiago.medassis@gmail.com>
2025-03-21 17:55:05 -0300

    Project setup

---
+++ app/urls.py
@@ -0,0 +1,15 @@
+from debug_toolbar.toolbar import debug_toolbar_urls
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+
+# Overriding AdminSite attributes.
+admin.site.site_header = admin.site.site_title = "GYMNASTICA"
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += debug_toolbar_urls()
